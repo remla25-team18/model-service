3bb668849ae24d9ebe9da380ab89648d1f0caeab
Yahor Dziomin <y.y.dziomin@student.tudelft.nl>
2025-05-13 21:42:44 +0200

    added version aware behavior

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
-FROM python:3.12-slim
+FROM python:3.11-slim
 
 WORKDIR /app
 
 COPY . /app
 
+RUN apt-get update && apt-get install -y git && rm -rf /var/lib/apt/lists/*
+RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-EXPOSE 5000
+EXPOSE 5050
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -2,10 +2,29 @@ from flask import Flask, request, jsonify
 from predict import predict_sentiment
 from model_loader import load_latest_model
 
+def get_current_version():
+    try:
+        version_file = open("VERSION", "r")
+        version = version_file.read().strip()
+        version_file.close()
+        # remove "-pre", decrease Patch version by 1
+        version = version.split("-")[0]
+        version_parts = version.split(".")
+        if len(version_parts) == 3:
+            major, minor, patch = version_parts
+            patch = str(int(patch) - 1)
+            version = f"{major}.{minor}.{patch}"
+        else:
+            raise ValueError("Version format is incorrect")
+        return version
+    except FileNotFoundError:
+        return None
+
 app = Flask(__name__)
 
 # Load latest model and vectorizer on startup
-model, vectorizer, version = load_latest_model()
+model, vectorizer, model_version = load_latest_model()
+version = get_current_version()
 
 @app.route("/test", methods=["GET"])
 def test():
@@ -18,7 +37,11 @@ def predict():
         return jsonify({"error": "Missing 'text' in request body"}), 400
 
     prediction = predict_sentiment(data["text"], model, vectorizer)
-    return jsonify({"prediction": int(prediction), "version": str(version)}), 200
+    return jsonify({
+        "prediction": int(prediction), 
+        "model_version": str(model_version),
+        "module_version": version or "No version found:("
+        }), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
